ed62966ba23a123ff82049fb18de5ac11e2dee43
mperalta1979 <marceloperaltavergara@gmail.com>
2026-08-19 11:27:26 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+// Subir este número cada vez que se actualice el código de la app
+const CACHE_VERSION = 'flujo-nv-v1';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE_VERSION).map((k) => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Network-first para que siempre se vea la versión más nueva cuando hay conexión;
+  // si falla (sin internet), cae al cache.
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_VERSION).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
